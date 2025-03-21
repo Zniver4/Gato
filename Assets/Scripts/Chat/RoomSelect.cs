ca7433747861817993c2277d6b89b7d9c840bb3f
Vicious Sholenzk <sholenzk@gmail.com>
2025-03-20 18:09:00 -0600

    Botones
    Ya están los botones de acceso a los chats y sus respectivos regresos a menú.

--- Assets/Scripts/Chat/RoomSelect.cs
+++ Assets/Scripts/Chat/RoomSelect.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class RoomSelect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject rooms;
+    public GameObject anime;
+    public GameObject movies;
+
+    public void Anime()
     {
+        Canvas room = rooms.GetComponent<Canvas>();
+        room.enabled = false;
         
+        Canvas anim = anime.GetComponent<Canvas>();
+        anim.enabled = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Movies()
     {
+        Canvas room = rooms.GetComponent<Canvas>();
+        room.enabled = false;
         
+        Canvas movie = movies.GetComponent<Canvas>();
+        movie.enabled = true;
+    }
+
+    public void BackAnime()
+    {
+        Canvas room = rooms.GetComponent<Canvas>();
+        room.enabled = true;
+        
+        Canvas anim = anime.GetComponent<Canvas>();
+        anim.enabled = false;
+    }
+    
+    public void BackMovies()
+    {
+        Canvas room = rooms.GetComponent<Canvas>();
+        room.enabled = true;
+        
+        Canvas movie = movies.GetComponent<Canvas>();
+        movie.enabled = false;
     }
 }
